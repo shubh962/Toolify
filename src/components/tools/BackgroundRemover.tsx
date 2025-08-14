73c088f36396883f7698ea5505428b6647299880
shubh962 <92972377+shubh962@users.noreply.github.com>
2025-08-14 20:57:42 +0530

    BackgroundRemover.tsx

--- src/components/tools/BackgroundRemover.tsx
+++ src/components/tools/BackgroundRemover.tsx
@@ -196,7 +196,7 @@ export default function BackgroundRemover() {
 }
 
 const Sparkles = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
+  <svg xmlns="https://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
     <path d="M9.93 13.5A2.25 2.25 0 0 0 12 12a2.25 2.25 0 0 0-2.07-1.5" />
     <path d="M12 2v2" />
     <path d="m4.93 4.93 1.41 1.41" />
