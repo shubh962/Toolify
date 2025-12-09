f500d635bcca22270abbd917c2e21f3871cdbfeb
shubh962 <92972377+shubh962@users.noreply.github.com>
2025-12-09 13:27:58 +0530

    Update genkit.ts

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,5 +3,5 @@ import { googleAI } from '@genkit-ai/googleai';
 
 export const ai = genkit({
   plugins: [googleAI()],
-  model: "googleai/gemini-2.0-flash"
+  model: "googleai/gemini-1.5-flash" // Maine 2.0 ko hata kar 1.5 kar diya hai
 });
